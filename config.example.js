63189ae41061e99b9b5572b4c0dac86f7f584dae
zhaofinger <zhaohaibo1994@gmail.com>
2018-03-14 12:54:33 +0800

    feat: 日程bot

--- config.example.js
+++ config.example.js
@@ -1,3 +1,34 @@
 module.exports = {
-  TG_TOKEN: 'xxxxxx:xxxxxxxxx'
+  TG_TOKEN: '123456789:abcdefghijklmnopqrstuvwxyz',
+  CHATID: '123456789',
+  SCHEDULE_LIST: [
+    {
+      time: '8:00',
+      message: '主人，该起床了！'
+    },
+    {
+      time: '8:55',
+      message: '主人，现在需要吃早饭以及打卡！'
+    },
+    {
+      time: '12:00',
+      message: '主人，该吃午饭了！'
+    },
+    {
+      time: '14:00',
+      message: '主人，午睡结束，开始下午的工作吧！'
+    },
+    {
+      time: '16:00',
+      message: '主人，休息一下！'
+    },
+    {
+      time: '18:30',
+      message: '主人，下班打卡了！'
+    },
+    {
+      time: '23:30',
+      message: '主人，睡觉睡觉！'
+    },
+  ]
 };
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const Telegraf = require('telegraf');
-const TG_TOKEN = require('./config').TG_TOKEN;
+const Telegram = require('telegraf/telegram');
+const schedule = require('node-schedule');
+
+const { TG_TOKEN, SCHEDULE_LIST, CHATID } = require('./config');              // tg token
 
 const app = new Telegraf(TG_TOKEN);
+const telegram = new Telegram(TG_TOKEN);
+
+// 定时任务
+const scheduleDict = {};
+SCHEDULE_LIST.forEach((item, index) => {
+  let timeArr = item.time.split(':');
+  scheduleDict['schedule' + index] = schedule.scheduleJob(`0 ${parseInt(timeArr[1], 10)} ${parseInt(timeArr[0], 10)} * * *`, () => {
+    telegram.sendMessage(CHATID, `现在时间是 ${new Date()} \n${item.message}`);
+  });
+});
+
 
 app.command('start', ctx => {
-  console.log('start', ctx.from);
   ctx.reply('Welcome!');
 });
-app.hears('hi', ctx => ctx.reply('Hey there!'));
-app.on('sticker', ctx => ctx.reply('👍'));
+
+// get chat id
+app.command('my_id', ctx => {
+  ctx.reply(`${ctx.from.username} chatId is ${ctx.from.id}`);
+});
+
 app.startPolling();
